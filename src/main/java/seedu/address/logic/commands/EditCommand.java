7657246f2c843edd19f2626042bbb201bd1a3e79
DarylTew <tew.you.wen.daryl@gmail.com>
2018-09-17 20:38:28 +0800

    Change Prefixes and descriptions of Message response messages
    Add [type/TYPE] [duration/DURATION] [difficulty/DIFFICULTY]
    [equipment/EQUIPMENT] [muscle/MUSCLE] [calories/CALORIES]
    [instruction/INSTRUCTION] into Prefixes
    Change descriptions of MESSAGE_EDIT_PERSON_SUCCESS and MESSAGE_DUPLICATE_PERSON
    from "person" to "workout"

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -1,10 +1,17 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+//import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+//import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+//import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TYPE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DURATION;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DIFFICULTY;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EQUIPMENT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MUSCLE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CALORIES;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_INSTRUCTION;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
@@ -28,28 +35,35 @@ import seedu.address.model.person.Phone;
 import seedu.address.model.tag.Tag;
 
 /**
- * Edits the details of an existing person in the address book.
+ * Edits the details of an existing workout in the address book.
  */
 public class EditCommand extends Command {
 
     public static final String COMMAND_WORD = "edit";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the details of the person identified "
-            + "by the index number used in the displayed person list. "
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the details of the workout identified "
+            + "by the index number used in the displayed workout list. "
             + "Existing values will be overwritten by the input values.\n"
             + "Parameters: INDEX (must be a positive integer) "
             + "[" + PREFIX_NAME + "NAME] "
-            + "[" + PREFIX_PHONE + "PHONE] "
-            + "[" + PREFIX_EMAIL + "EMAIL] "
-            + "[" + PREFIX_ADDRESS + "ADDRESS] "
+//            + "[" + PREFIX_PHONE + "PHONE] "
+//            + "[" + PREFIX_EMAIL + "EMAIL] "
+//            + "[" + PREFIX_ADDRESS + "ADDRESS] "
+            + "[" + PREFIX_TYPE + "TYPE] "
+            + "[" + PREFIX_DURATION + "DURATION] "
+            + "[" + PREFIX_DIFFICULTY + "DIFFICULTY] "
+            + "[" + PREFIX_EQUIPMENT + "EQUIPMENT] "
+            + "[" + PREFIX_MUSCLE + "MUSCLE] "
+            + "[" + PREFIX_CALORIES + "CALORIES] "
+            + "[" + PREFIX_INSTRUCTION + "INSTRUCTION] "
             + "[" + PREFIX_TAG + "TAG]...\n"
             + "Example: " + COMMAND_WORD + " 1 "
-            + PREFIX_PHONE + "91234567 "
-            + PREFIX_EMAIL + "johndoe@example.com";
+            + PREFIX_TYPE + "Anaerobic "
+            + PREFIX_DIFFICULTY + "Beginner";
 
-    public static final String MESSAGE_EDIT_PERSON_SUCCESS = "Edited Person: %1$s";
+    public static final String MESSAGE_EDIT_PERSON_SUCCESS = "Edited Workout: %1$s";
     public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
-    public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book.";
+    public static final String MESSAGE_DUPLICATE_PERSON = "This workout already exists in the address book.";
 
     private final Index index;
     private final EditPersonDescriptor editPersonDescriptor;
--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -7,9 +7,16 @@ public class CliSyntax {
 
     /* Prefix definitions */
     public static final Prefix PREFIX_NAME = new Prefix("n/");
-    public static final Prefix PREFIX_PHONE = new Prefix("p/");
-    public static final Prefix PREFIX_EMAIL = new Prefix("e/");
-    public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
+//    public static final Prefix PREFIX_PHONE = new Prefix("p/");
+//    public static final Prefix PREFIX_EMAIL = new Prefix("e/");
+//    public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
+    public static final Prefix PREFIX_TYPE = new Prefix("t/");
+    public static final Prefix PREFIX_DURATION = new Prefix("du/");
+    public static final Prefix PREFIX_DIFFICULTY = new Prefix("di/");
+    public static final Prefix PREFIX_EQUIPMENT = new Prefix("e/");
+    public static final Prefix PREFIX_MUSCLE = new Prefix("m/");
+    public static final Prefix PREFIX_CALORIES = new Prefix("c/");
+    public static final Prefix PREFIX_INSTRUCTION = new Prefix("i/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
 
 }
