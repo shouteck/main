269d4c16277b87920075d0584ab13e3845ac9300
DarylTew <tew.you.wen.daryl@gmail.com>
2018-09-17 21:45:29 +0800

    Change EditPersonDescriptor class and Add the classes
    Add [type/TYPE] [duration/DURATION] [difficulty/DIFFICULTY]
    [equipment/EQUIPMENT] [muscle/MUSCLE] [calories/CALORIES]
    [instruction/INSTRUCTION] into methods

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -27,11 +27,18 @@ import seedu.address.commons.util.CollectionUtil;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.person.Address;
-import seedu.address.model.person.Email;
-import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
-import seedu.address.model.person.Phone;
+import seedu.address.model.person.Name;
+//import seedu.address.model.person.Address;
+//import seedu.address.model.person.Email;
+//import seedu.address.model.person.Phone;
+import seedu.address.model.person.Type;
+import seedu.address.model.person.Duration;
+import seedu.address.model.person.Difficulty;
+import seedu.address.model.person.Equipment;
+import seedu.address.model.person.Muscle;
+import seedu.address.model.person.Calories;
+import seedu.address.model.person.Instruction;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -110,12 +117,20 @@ public class EditCommand extends Command {
         assert personToEdit != null;
 
         Name updatedName = editPersonDescriptor.getName().orElse(personToEdit.getName());
-        Phone updatedPhone = editPersonDescriptor.getPhone().orElse(personToEdit.getPhone());
-        Email updatedEmail = editPersonDescriptor.getEmail().orElse(personToEdit.getEmail());
-        Address updatedAddress = editPersonDescriptor.getAddress().orElse(personToEdit.getAddress());
+//        Phone updatedPhone = editPersonDescriptor.getPhone().orElse(personToEdit.getPhone());
+//        Email updatedEmail = editPersonDescriptor.getEmail().orElse(personToEdit.getEmail());
+//        Address updatedAddress = editPersonDescriptor.getAddress().orElse(personToEdit.getAddress());
+        Type updatedType = editPersonDescriptor.getType().orElse(personToEdit.getType());
+        Duration updatedDuration = editPersonDescriptor.getDuration().orElse(personToEdit.getDuration());
+        Difficulty updatedDifficulty = editPersonDescriptor.getDifficulty().orElse(personToEdit.getDifficulty());
+        Equipment updatedEquipment = editPersonDescriptor.getEquipment().orElse(personToEdit.getEquipment());
+        Muscle updatedMuscle = editPersonDescriptor.getMuscle().orElse(personToEdit.getMuscle());
+        Calories updatedCalories = editPersonDescriptor.getCalories().orElse(personToEdit.getCalories());
+        Instruction updatedInstruction = editPersonDescriptor.getInstruction().orElse(personToEdit.getInstruction());
         Set<Tag> updatedTags = editPersonDescriptor.getTags().orElse(personToEdit.getTags());
 
-        return new Person(updatedName, updatedPhone, updatedEmail, updatedAddress, updatedTags);
+        return new Person(updatedName, updatedType, updatedDuration, updatedDifficulty, updatedEquipment,
+        updatedMuscle, updatedCalories, updatedInstruction, updatedTags);
     }
 
     @Override
@@ -142,9 +157,16 @@ public class EditCommand extends Command {
      */
     public static class EditPersonDescriptor {
         private Name name;
-        private Phone phone;
-        private Email email;
-        private Address address;
+//        private Phone phone;
+//        private Email email;
+//        private Address address;
+        private Type type;
+        private Duration duration;
+        private Difficulty difficulty;
+        private Equipment equipment;
+        private Muscle muscle;
+        private Calories calories;
+        private Instruction instruction;
         private Set<Tag> tags;
 
         public EditPersonDescriptor() {}
@@ -155,9 +177,16 @@ public class EditCommand extends Command {
          */
         public EditPersonDescriptor(EditPersonDescriptor toCopy) {
             setName(toCopy.name);
-            setPhone(toCopy.phone);
-            setEmail(toCopy.email);
-            setAddress(toCopy.address);
+//            setPhone(toCopy.phone);
+//            setEmail(toCopy.email);
+//            setAddress(toCopy.address);
+            setType(toCopy.type);
+            setDuration(toCopy.duration);
+            setDifficulty(toCopy.difficulty);
+            setEquipment(toCopy.equipment);
+            setMuscle(toCopy.muscle);
+            setCalories(toCopy.calories);
+            setInstruction(toCopy.instruction);
             setTags(toCopy.tags);
         }
 
@@ -165,7 +194,8 @@ public class EditCommand extends Command {
          * Returns true if at least one field is edited.
          */
         public boolean isAnyFieldEdited() {
-            return CollectionUtil.isAnyNonNull(name, phone, email, address, tags);
+            return CollectionUtil.isAnyNonNull(name, type, duration, difficulty, equipment, muscle, calories,
+            instruction, tags);
         }
 
         public void setName(Name name) {
@@ -176,29 +206,85 @@ public class EditCommand extends Command {
             return Optional.ofNullable(name);
         }
 
-        public void setPhone(Phone phone) {
-            this.phone = phone;
+        public void setType(Type type) {
+            this.type = type;
         }
 
-        public Optional<Phone> getPhone() {
-            return Optional.ofNullable(phone);
+        public Optional<Type> getType() {
+            return Optional.ofNullable(type);
         }
 
-        public void setEmail(Email email) {
-            this.email = email;
+        public void setDuration(Duration duration) {
+            this.duration = duration;
         }
 
-        public Optional<Email> getEmail() {
-            return Optional.ofNullable(email);
+        public Optional<Duration> getDuration() {
+            return Optional.ofNullable(duration);
         }
 
-        public void setAddress(Address address) {
-            this.address = address;
+        public void setDifficulty(Difficulty difficulty) {
+            this.difficulty = difficulty;
         }
 
-        public Optional<Address> getAddress() {
-            return Optional.ofNullable(address);
+        public Optional<Difficulty> getDifficulty() {
+            return Optional.ofNullable(difficulty);
         }
+
+        public void setEquipment(Equipment equipment) {
+            this.equipment = equipment;
+        }
+
+        public Optional<Equipment> getEquipment() {
+            return Optional.ofNullable(equipment);
+        }
+
+        public void setMuscle(Muscle muscle) {
+            this.muscle = muscle;
+        }
+
+        public Optional<Muscle> getMuscle() {
+            return Optional.ofNullable(muscle);
+        }
+
+        public void setCalories(Calories calories) {
+            this.calories = calories;
+        }
+
+        public Optional<Calories> getCalories() {
+            return Optional.ofNullable(calories);
+        }
+
+        public void setInstruction(Instruction instruction) {
+            this.instruction = instruction;
+        }
+
+        public Optional<Instruction> getInstruction() {
+            return Optional.ofNullable(instruction);
+        }
+
+//        public void setPhone(Phone phone) {
+//            this.phone = phone;
+//        }
+//
+//        public Optional<Phone> getPhone() {
+//            return Optional.ofNullable(phone);
+//        }
+//
+//        public void setEmail(Email email) {
+//            this.email = email;
+//        }
+//
+//        public Optional<Email> getEmail() {
+//            return Optional.ofNullable(email);
+//        }
+//
+//        public void setAddress(Address address) {
+//            this.address = address;
+//        }
+//
+//        public Optional<Address> getAddress() {
+//            return Optional.ofNullable(address);
+//        }
 
         /**
          * Sets {@code tags} to this object's {@code tags}.
@@ -233,9 +319,16 @@ public class EditCommand extends Command {
             EditPersonDescriptor e = (EditPersonDescriptor) other;
 
             return getName().equals(e.getName())
-                    && getPhone().equals(e.getPhone())
-                    && getEmail().equals(e.getEmail())
-                    && getAddress().equals(e.getAddress())
+//                    && getPhone().equals(e.getPhone())
+//                    && getEmail().equals(e.getEmail())
+//                    && getAddress().equals(e.getAddress())
+                    && getType().equals(e.getType())
+                    && getDuration().equals(e.getDuration())
+                    && getDifficulty().equals(e.getDifficulty())
+                    && getEquipment().equals(e.getEquipment())
+                    && getMuscle().equals(e.getMuscle())
+                    && getCalories().equals(e.getCalories())
+                    && getInstruction().equals(e.getInstruction())
                     && getTags().equals(e.getTags());
         }
     }
---
+++ src/main/java/seedu/address/model/person/Instruction.java
@@ -0,0 +1,57 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Workout's Instruction in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidInstruction(String)}
+ */
+public class Instruction {
+
+    public static final String MESSAGE_INSTRUCTION_CONSTRAINTS =
+            "Instruction should only contain alphanumeric characters and spaces, and it should not be blank";
+
+    /*
+     * The first character of the address must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String INSTRUCTION_VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+
+    public final String fullInstruction;
+
+    /**
+     * Constructs a {@code Instruction}.
+     *
+     * @param instruction A valid instruction.
+     */
+    public Instruction(String instruction) {
+        requireNonNull(instruction);
+        checkArgument(isValidInstruction(instruction), MESSAGE_INSTRUCTION_CONSTRAINTS);
+        fullInstruction = instruction;
+    }
+
+    /**
+     * Returns true if a given string is a valid name.
+     */
+    public static boolean isValidInstruction(String test) { return test.matches(INSTRUCTION_VALIDATION_REGEX); }
+
+
+    @Override
+    public String toString() {
+        return fullInstruction;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Instruction // instanceof handles nulls
+                && fullInstruction.equals(((Instruction) other).fullInstruction)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return fullInstruction.hashCode();
+    }
+
+}
