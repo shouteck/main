b75fd96b0f7229d20110768fc62ed0baf0410b5d
shouteck <shouteckie@gmail.com>
2018-09-19 17:01:09 +0800

    Changed MESSAGE_USAGE in AddCommand and added our new prefixes to CliSyntax.

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -6,17 +6,14 @@ package seedu.address.logic.parser;
 public class CliSyntax {
 
     /* Prefix definitions */
-    public static final Prefix PREFIX_NAME = new Prefix("n/");
-//    public static final Prefix PREFIX_PHONE = new Prefix("p/");
-//    public static final Prefix PREFIX_EMAIL = new Prefix("e/");
-//    public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
-    public static final Prefix PREFIX_TYPE = new Prefix("t/");
-    public static final Prefix PREFIX_DURATION = new Prefix("du/");
-    public static final Prefix PREFIX_DIFFICULTY = new Prefix("di/");
-    public static final Prefix PREFIX_EQUIPMENT = new Prefix("e/");
-    public static final Prefix PREFIX_MUSCLE = new Prefix("m/");
-    public static final Prefix PREFIX_CALORIES = new Prefix("c/");
-    public static final Prefix PREFIX_INSTRUCTION = new Prefix("i/");
-    public static final Prefix PREFIX_TAG = new Prefix("t/");
+    public static final Prefix PREFIX_NAME = new Prefix("name/");
+    public static final Prefix PREFIX_TYPE = new Prefix("type/");
+    public static final Prefix PREFIX_DURATION = new Prefix("duration/");
+    public static final Prefix PREFIX_DIFFICULTY = new Prefix("difficulty/");
+    public static final Prefix PREFIX_EQUIPMENT = new Prefix("equipment/");
+    public static final Prefix PREFIX_MUSCLE = new Prefix("muscle/");
+    public static final Prefix PREFIX_CALORIES = new Prefix("calories/");
+    public static final Prefix PREFIX_INSTRUCTION = new Prefix("instruction/");
+    public static final Prefix PREFIX_TAG = new Prefix("tag/");
 
 }
