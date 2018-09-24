942cd5dc2f7474e737757fc2adc4611e0fb51174
SJ <dindis123@hotmail.com>
2018-09-24 16:08:11 +0800

    Update java\seedu\address\model Change Address -> Workout Change Person -> Workout Change all the import files @java\seedu\address\model

--- src/main/java/seedu/address/model/workout/Workout.java
+++ src/main/java/seedu/address/model/workout/Workout.java
@@ -10,7 +10,7 @@ import java.util.Set;
 import seedu.address.model.tag.Tag;
 
 /**
- * Represents a Workout in the workout book.
+ * Represents a Workout in the workout book. 
  * Guarantees: details are present and not null, field values are validated, immutable.
  */
 public class Workout {
