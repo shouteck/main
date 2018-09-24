584ebb6bc6f14bb729dceb72a14f82a908efbf74
DarylTew <tew.you.wen.daryl@gmail.com>
2018-09-24 20:53:20 +0800

    Fix typos
    Morph person into workout

--- src/test/java/seedu/address/logic/commands/EditWorkoutDescriptorTest.java
+++ src/test/java/seedu/address/logic/commands/EditWorkoutDescriptorTest.java
@@ -2,27 +2,20 @@ package seedu.address.logic.commands;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static seedu.address.logic.commands.CommandTestUtil.DESC_AMY_WORKOOUT;
+import static seedu.address.logic.commands.CommandTestUtil.DESC_AMY_WORKOUT;
 import static seedu.address.logic.commands.CommandTestUtil.DESC_BOB_WORKOUT;
-//import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
-//import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
-//import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
-//import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
-//import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB_WORKOUT;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TYPE_BOB_WORKOUT
-import static seedu.address.logic.commands.CommandTestUtil.VALID_DURATION_BOB_WORKOUT
-import static seedu.address.logic.commands.CommandTestUtil.VALID_DIFFICULTY_BOB_WORKOUT
-import static seedu.address.logic.commands.CommandTestUtil.VALID_EQUIPMENT_BOB_WORKOUT
-import static seedu.address.logic.commands.CommandTestUtil.VALID_MUSCLE_BOB_WORKOUT
-import static seedu.address.logic.commands.CommandTestUtil.VALID_CALORIES_BOB_WORKOUT
-import static seedu.address.logic.commands.CommandTestUtil.VALID_INSTRUCTION_BOB_WORKOUT
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TYPE_BOB_WORKOUT;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_DURATION_BOB_WORKOUT;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_DIFFICULTY_BOB_WORKOUT;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_EQUIPMENT_BOB_WORKOUT;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_MUSCLE_BOB_WORKOUT;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_CALORIES_BOB_WORKOUT;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_INSTRUCTION_BOB_WORKOUT;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_NIGHT;
 
 import org.junit.Test;
 
-//import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
-//import seedu.address.testutil.EditPersonDescriptorBuilder;
 import seedu.address.logic.commands.EditCommand.EditWorkoutDescriptor;
 import seedu.address.testutil.EditWorkoutDescriptorBuilder;
 
