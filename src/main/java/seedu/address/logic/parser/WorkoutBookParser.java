ef663d111bc57272402458a613853360d4e16413
yicheng12 <2731705945zuigao@gmail.com>
2018-10-04 01:56:16 +0800

    add SortCommand have not worked yet

--- src/main/java/seedu/address/logic/parser/WorkoutBookParser.java
+++ src/main/java/seedu/address/logic/parser/WorkoutBookParser.java
@@ -21,6 +21,7 @@ import seedu.address.logic.commands.RecommendCommand;
 import seedu.address.logic.commands.RedoCommand;
 import seedu.address.logic.commands.SelectCommand;
 import seedu.address.logic.commands.UndoCommand;
+import seedu.address.logic.commands.SortCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
@@ -91,6 +92,9 @@ public class WorkoutBookParser {
 
             case RedoCommand.COMMAND_WORD:
                 return new RedoCommand();
+
+            case SortCommand.COMMAND_WORD:
+                return new SortCommand();
 
             default:
                 throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
