069c817830ce6258de429a9aad0e508218e53fbc
garyheng <E0026809@u.nus.edu>
2018-10-03 23:26:01 +0800

    Add TrackCommand, TrackCommandTest
    Change WorkoutBookParser & Test to accept Track command

---
+++ src/test/java/seedu/address/logic/commands/TrackCommandTest.java
@@ -0,0 +1,24 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.TrackCommand.MESSAGE_NOT_IMPLEMENTED_YET;
+import static seedu.address.testutil.TypicalWorkouts.getTypicalWorkoutBook;
+
+import org.junit.Test;
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+/**
+ * Contains unit tests for TrackCommand
+ */
+public class TrackCommandTest {
+
+    private Model model = new ModelManager(getTypicalWorkoutBook(), new UserPrefs());
+
+    @Test
+    public void execute(){
+        assertCommandFailure(new TrackCommand(), model, new CommandHistory(), MESSAGE_NOT_IMPLEMENTED_YET);
+    }
+}
