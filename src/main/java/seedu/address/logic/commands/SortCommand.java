7294dcc6c66b80bd12f37024e4215b4ba399384c
shouteck <shouteckie@gmail.com>
2018-10-15 23:30:38 +0800

    Fix some travis errors

--- src/main/java/seedu/address/logic/commands/SortCommand.java
+++ src/main/java/seedu/address/logic/commands/SortCommand.java
@@ -1,19 +1,14 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import java.util.Collections;
-import java.util.List;
 
-import seedu.address.commons.core.Messages;
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
-import seedu.address.model.workout.Workout;
 
 public class SortCommand extends Command {
     public static final String COMMAND_WORD = "sort";
 
     public static final String MESSAGE_USAGE = "sorted all workouts";
-
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) {
