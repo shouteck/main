391ab411a301bf6ba1650a623738c25b5a9a8402
DarylTew <tew.you.wen.daryl@gmail.com>
2018-10-18 12:36:39 +0800

    Fix WorkoutBookParser
    Missing case of CurrentCommand and CompleteCommand

--- src/main/java/seedu/address/logic/parser/WorkoutBookParser.java
+++ src/main/java/seedu/address/logic/parser/WorkoutBookParser.java
@@ -7,25 +7,7 @@ import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import seedu.address.logic.commands.AddCommand;
-import seedu.address.logic.commands.ClearCommand;
-import seedu.address.logic.commands.Command;
-import seedu.address.logic.commands.DeleteCommand;
-import seedu.address.logic.commands.EditCommand;
-import seedu.address.logic.commands.ExitCommand;
-import seedu.address.logic.commands.FilterCommand;
-import seedu.address.logic.commands.FindCommand;
-import seedu.address.logic.commands.HelpCommand;
-import seedu.address.logic.commands.HistoryCommand;
-import seedu.address.logic.commands.ListCommand;
-import seedu.address.logic.commands.ModifyCommand;
-import seedu.address.logic.commands.ProfileCommand;
-import seedu.address.logic.commands.RecommendCommand;
-import seedu.address.logic.commands.RedoCommand;
-import seedu.address.logic.commands.SelectCommand;
-import seedu.address.logic.commands.SortCommand;
-import seedu.address.logic.commands.TrackCommand;
-import seedu.address.logic.commands.UndoCommand;
+import seedu.address.logic.commands.*;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
@@ -60,6 +42,12 @@ public class WorkoutBookParser {
 
         case EditCommand.COMMAND_WORD:
             return new EditCommandParser().parse(arguments);
+
+        case CurrentCommand.COMMAND_WORD:
+            return new CurrentCommandParser().parse(arguments);
+
+        case CompleteCommand.COMMAND_WORD:
+            return new CompleteCommandParser().parse(arguments);
 
         case SelectCommand.COMMAND_WORD:
             return new SelectCommandParser().parse(arguments);
