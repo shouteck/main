72641d35f428a44a3b158bcf56abfa6c7a2e1e4d
shouteck <shouteckie@gmail.com>
2018-10-27 04:47:22 +0800

    Update Calories REGEX to [0,1000] Update Duration REGEX to [0m,1000m] Update RecommendCommandParser and RecommendCommand Tests Update a new section in CommandFormat in User Guide on the parameters

--- src/test/java/seedu/address/logic/parser/RecommendCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/RecommendCommandParserTest.java
@@ -22,53 +22,93 @@ public class RecommendCommandParserTest {
 
     @Test
     public void parse_optionalFieldsMissing_success() {
-        // Missing Difficulty and Duration fields
-        Optional<Calories> calories = Optional.of(new Calories("150"));
+        // Valid Calories without Difficulty and Duration fields
+        Optional<Calories> calories = Optional.of(new Calories("1"));
         RecommendArguments expectedRecommendArguments = new RecommendArguments.Builder().withCalories(calories).build();
-        assertParseSuccess(parser, " " + PREFIX_CALORIES + "150",
+        assertParseSuccess(parser, " " + PREFIX_CALORIES + "1",
                 new RecommendCommand(expectedRecommendArguments));
 
-        // Missing Calories and Duration fields
+        // Valid Difficulty without Calories and Duration fields
         Optional<Difficulty> difficulty = Optional.of(new Difficulty("beginner"));
         expectedRecommendArguments = new RecommendArguments.Builder().withDifficulty(difficulty).build();
         assertParseSuccess(parser, " " + PREFIX_DIFFICULTY + "beginner",
                 new RecommendCommand(expectedRecommendArguments));
 
-        // Missing Calories and Difficulty fields
-        Optional<Duration> duration = Optional.of(new Duration("10m"));
+        // Valid Duration without Calories and Difficulty fields
+        Optional<Duration> duration = Optional.of(new Duration("1m"));
         expectedRecommendArguments = new RecommendArguments.Builder().withDuration(duration).build();
-        assertParseSuccess(parser, " " + PREFIX_DURATION + "10m",
+        assertParseSuccess(parser, " " + PREFIX_DURATION + "1m",
                 new RecommendCommand(expectedRecommendArguments));
 
-        // Missing Duration field
-        calories = Optional.of(new Calories("150"));
-        difficulty = Optional.of(new Difficulty("beginner"));
+        // Valid Calories and Difficulty without Duration field
+        calories = Optional.of(new Calories("1000"));
+        difficulty = Optional.of(new Difficulty("intermediate"));
         expectedRecommendArguments = new RecommendArguments.Builder().withCalories(calories)
                 .withDifficulty(difficulty).build();
-        assertParseSuccess(parser, " " + PREFIX_CALORIES + "150" + " " + PREFIX_DIFFICULTY + "beginner",
+        assertParseSuccess(parser, " " + PREFIX_CALORIES + "1000" + " " + PREFIX_DIFFICULTY + "intermediate",
                 new RecommendCommand(expectedRecommendArguments));
 
-        // Missing Difficulty field
-        calories = Optional.of(new Calories("150"));
-        duration = Optional.of(new Duration("10m"));
+        // Valid Calories and Duration without Difficulty field
+        calories = Optional.of(new Calories("500"));
+        duration = Optional.of(new Duration("1000m"));
         expectedRecommendArguments = new RecommendArguments.Builder().withCalories(calories)
                 .withDuration(duration).build();
-        assertParseSuccess(parser, " " + PREFIX_CALORIES + "150" + " " + PREFIX_DURATION + "10m",
+        assertParseSuccess(parser, " " + PREFIX_CALORIES + "500" + " " + PREFIX_DURATION + "1000m",
                 new RecommendCommand(expectedRecommendArguments));
 
-        // Missing Calories Field
-        difficulty = Optional.of(new Difficulty("beginner"));
-        duration = Optional.of(new Duration("10m"));
+        // Valid Difficulty and Duration without Calories Field
+        difficulty = Optional.of(new Difficulty("advanced"));
+        duration = Optional.of(new Duration("500m"));
         expectedRecommendArguments = new RecommendArguments.Builder().withDifficulty(difficulty)
                 .withDuration(duration).build();
-        assertParseSuccess(parser, " " + PREFIX_DIFFICULTY + "beginner" + " " + PREFIX_DURATION + "10m",
+        assertParseSuccess(parser, " " + PREFIX_DIFFICULTY + "advanced" + " " + PREFIX_DURATION + "500m",
                 new RecommendCommand(expectedRecommendArguments));
     }
 
     @Test
+    public void parse_allFieldsPresent_success() {
+        // Valid Calories,Difficulty and Duration fields
+        Optional<Calories> calories = Optional.of(new Calories("750"));
+        Optional<Difficulty> difficulty = Optional.of(new Difficulty("intermediate"));
+        Optional<Duration> duration = Optional.of(new Duration("750m"));
+        RecommendArguments expectedRecommendArguments = new RecommendArguments.Builder().withCalories(calories)
+                .withDifficulty(difficulty).withDuration(duration).build();
+        assertParseSuccess(parser, " " + PREFIX_CALORIES + "750" + " " + PREFIX_DIFFICULTY + "intermediate"
+                        + " " + PREFIX_DURATION + "750m",
+                new RecommendCommand(expectedRecommendArguments));
+    }
+
+    @Test
+    public void parse_invalidValue_failure() {
+        // Invalid Calories < 1
+        assertParseFailure(parser, " " + PREFIX_CALORIES + "0", Calories.MESSAGE_CALORIES_CONSTRAINTS);
+
+        // Invalid Calories > 1000
+        assertParseFailure(parser, " " + PREFIX_CALORIES + "1001", Calories.MESSAGE_CALORIES_CONSTRAINTS);
+
+        // Invalid Calories for non-integer
+        assertParseFailure(parser, " " + PREFIX_CALORIES + "hundred", Calories.MESSAGE_CALORIES_CONSTRAINTS);
+
+        // Invalid Difficulty for non-case sensitive inputs
+        assertParseFailure(parser, " " + PREFIX_DIFFICULTY + "beginNer"
+                , Difficulty.MESSAGE_DIFFICULTY_CONSTRAINTS);
+
+        // Invalid Difficulty for integer
+        assertParseFailure(parser, " " + PREFIX_DIFFICULTY + "1", Difficulty.MESSAGE_DIFFICULTY_CONSTRAINTS);
+
+        // Invalid Duration for < 1m
+        assertParseFailure(parser, " " + PREFIX_DURATION + "0m", Duration.MESSAGE_DURATION_CONSTRAINTS);
+
+        // Invalid Duration for > 1000m
+        assertParseFailure(parser, " " + PREFIX_DURATION + "1001m", Duration.MESSAGE_DURATION_CONSTRAINTS);
+
+        // Invalid Duration for non-integer
+        assertParseFailure(parser, " " + PREFIX_DURATION + "fivem", Duration.MESSAGE_DURATION_CONSTRAINTS);
+    }
+
+    @Test
     public void parse_compulsoryFieldMissing_failure() {
-        String expectedMessage = String.format(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                RecommendCommand.MESSAGE_USAGE));
+        String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, RecommendCommand.MESSAGE_USAGE);
 
         // Missing Calories, Difficulty and Duration fields
         assertParseFailure(parser, " ", expectedMessage);
