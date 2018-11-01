296511f2aeb22a49e6473d44fb0a51e248758377
DarylTew <tew.you.wen.daryl@gmail.com>
2018-11-01 23:17:02 +0800

    Add exception to EditCommand

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -62,6 +62,8 @@ public class EditCommand extends Command {
     public static final String MESSAGE_EDIT_WORKOUT_SUCCESS = "Edited Workout: %1$s";
     public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
     public static final String MESSAGE_DUPLICATE_WORKOUT = "This workout already exists in the workout book.";
+    public static final String MESSAGE_TAG_ERROR = "Tags which are to be edited in cannot be future, current or"
+            + "completed.";
 
     private final Index index;
     private final EditWorkoutDescriptor editWorkoutDescriptor;
@@ -104,7 +106,8 @@ public class EditCommand extends Command {
      * Creates and returns a {@code Workout} with the details of {@code workoutToEdit}
      * edited with {@code editWorkoutDescriptor}.
      */
-    private static Workout createEditedWorkout(Workout workoutToEdit, EditWorkoutDescriptor editWorkoutDescriptor) {
+    private static Workout createEditedWorkout(Workout workoutToEdit, EditWorkoutDescriptor editWorkoutDescriptor)
+            throws CommandException {
         assert workoutToEdit != null;
 
         Name updatedName = editWorkoutDescriptor.getName().orElse(workoutToEdit.getName());
@@ -131,6 +134,7 @@ public class EditCommand extends Command {
         for (Tag entry: originalTags) {
             if (entry.tagName.equals("future") || entry.tagName.equals("current")
                 || entry.tagName.equals("completed")) {
+                throw new CommandException(MESSAGE_TAG_ERROR);
             } else {
                 updatedTags.add(entry);
             }
