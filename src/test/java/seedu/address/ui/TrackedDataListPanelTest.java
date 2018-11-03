e0f7cc0bb2b502c720b43c2b014b21a2566a8f13
garyheng <E0026809@u.nus.edu>
2018-11-04 01:20:02 +0800

    Fix Travis Checks

--- src/test/java/seedu/address/ui/TrackedDataListPanelTest.java
+++ src/test/java/seedu/address/ui/TrackedDataListPanelTest.java
@@ -3,11 +3,9 @@ package seedu.address.ui;
 import static java.time.Duration.ofMillis;
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
-import static seedu.address.testutil.EventsUtil.postNow;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_WORKOUT;
 import static seedu.address.testutil.TypicalParameters.getTypicalParameters;
 import static seedu.address.ui.testutil.GuiTestAssert.assertCardDisplaysParameter;
-import static seedu.address.ui.testutil.GuiTestAssert.assertCardEquals;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
