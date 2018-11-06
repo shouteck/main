1238b5416b70d04fe923cbe8248a58c11f9f4a80
DarylTew <tew.you.wen.daryl@gmail.com>
2018-11-07 01:26:50 +0800

    Fix travis errors temporarily

--- src/test/java/seedu/address/logic/commands/CurrentCommandTest.java
+++ src/test/java/seedu/address/logic/commands/CurrentCommandTest.java
@@ -3,10 +3,10 @@ package seedu.address.logic.commands;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+//import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.showWorkoutAtIndex;
 import static seedu.address.logic.commands.CurrentCommand.MESSAGE_MULTIPLE_CURRENT_WORKOUT;
-import static seedu.address.logic.commands.CurrentCommand.createEditedWorkout;
+//import static seedu.address.logic.commands.CurrentCommand.createEditedWorkout;
 import static seedu.address.testutil.TypicalIndexes.INDEX_EIGHTH_WORKOUT;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_WORKOUT;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_WORKOUT;
@@ -19,13 +19,13 @@ import org.junit.Test;
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
-import seedu.address.logic.commands.exceptions.CommandException;
+//import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.TrackedData;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.WorkoutBook;
-import seedu.address.model.workout.Workout;
+//import seedu.address.model.WorkoutBook;
+//import seedu.address.model.workout.Workout;
 
 /**
  * Contains integration tests (interaction with the Model, UndoCommand and RedoCommand) and unit tests for
