b90de131a474a53a0515e75e6583c4a062384bb7
sijieg <dindis123@hotmail.com>
2018-11-06 16:10:55 +0800

    Fix travis errors

--- src/test/java/seedu/address/logic/parser/ModifyCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ModifyCommandParserTest.java
@@ -1,20 +1,20 @@
 package seedu.address.logic.parser;
 
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_CALORIES;
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_DURATION;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_DIFFICULTY;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_DURATION;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_GENDER;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_HEIGHT;
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_WEIGHT;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_USERNAME;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_WEIGHT;
 import static seedu.address.commons.core.Messages.MESSAGE_VALID_CALORIES;
-import static seedu.address.commons.core.Messages.MESSAGE_VALID_DURATION;
 import static seedu.address.commons.core.Messages.MESSAGE_VALID_DIFFICULTY;
+import static seedu.address.commons.core.Messages.MESSAGE_VALID_DURATION;
 import static seedu.address.commons.core.Messages.MESSAGE_VALID_GENDER;
 import static seedu.address.commons.core.Messages.MESSAGE_VALID_HEIGHT;
-import static seedu.address.commons.core.Messages.MESSAGE_VALID_WEIGHT;
 import static seedu.address.commons.core.Messages.MESSAGE_VALID_USERNAME;
+import static seedu.address.commons.core.Messages.MESSAGE_VALID_WEIGHT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 
 import org.junit.Test;
@@ -22,7 +22,7 @@ import org.junit.Test;
 import seedu.address.logic.commands.ModifyCommand;
 
 public class ModifyCommandParserTest {
-    ModifyCommandParser parser = new ModifyCommandParser();
+    private ModifyCommandParser parser = new ModifyCommandParser();
 
     @Test
     public void parse_emptyArg_throwsParseException() {
@@ -31,53 +31,53 @@ public class ModifyCommandParserTest {
     }
 
     @Test
-    public void parse_InvalidField_failure() {
+    public void parse_invalidField_failure() {
 
         // invalid calories field specified
-        assertParseFailure(parser," calories/150.2", String.format(MESSAGE_INVALID_CALORIES,
+        assertParseFailure(parser, " calories/150.2", String.format(MESSAGE_INVALID_CALORIES,
                 MESSAGE_VALID_CALORIES));
 
         // invalid difficulty field specified
-        assertParseFailure(parser," difficulty/easy", String.format(MESSAGE_INVALID_DIFFICULTY,
+        assertParseFailure(parser, " difficulty/easy", String.format(MESSAGE_INVALID_DIFFICULTY,
                 MESSAGE_VALID_DIFFICULTY));
 
         // invalid duration field specified
-        assertParseFailure(parser," duration/30", String.format(MESSAGE_INVALID_DURATION,
+        assertParseFailure(parser, " duration/30", String.format(MESSAGE_INVALID_DURATION,
                 MESSAGE_VALID_DURATION));
 
         // invalid username field specified
-        assertParseFailure(parser," username/John@Doe", String.format(MESSAGE_INVALID_USERNAME,
+        assertParseFailure(parser, " username/John@Doe", String.format(MESSAGE_INVALID_USERNAME,
                 MESSAGE_VALID_USERNAME));
     }
 
     @Test
-    public void parse_EmptyField_failure() {
+    public void parse_emptyField_failure() {
 
         // empty gender field specified
-        assertParseFailure(parser," gender/", String.format(MESSAGE_INVALID_GENDER, MESSAGE_VALID_GENDER));
+        assertParseFailure(parser, " gender/", String.format(MESSAGE_INVALID_GENDER, MESSAGE_VALID_GENDER));
 
         // empty calories field specified
-        assertParseFailure(parser," calories/", String.format(MESSAGE_INVALID_CALORIES,
+        assertParseFailure(parser, " calories/", String.format(MESSAGE_INVALID_CALORIES,
                 MESSAGE_VALID_CALORIES));
 
         // empty difficulty field specified
-        assertParseFailure(parser," difficulty/", String.format(MESSAGE_INVALID_DIFFICULTY,
+        assertParseFailure(parser, " difficulty/", String.format(MESSAGE_INVALID_DIFFICULTY,
                 MESSAGE_VALID_DIFFICULTY));
 
         // empty duration field specified
-        assertParseFailure(parser," duration/", String.format(MESSAGE_INVALID_DURATION,
+        assertParseFailure(parser, " duration/", String.format(MESSAGE_INVALID_DURATION,
                 MESSAGE_VALID_DURATION));
 
         // empty height field specified
-        assertParseFailure(parser," height/", String.format(MESSAGE_INVALID_HEIGHT,
+        assertParseFailure(parser, " height/", String.format(MESSAGE_INVALID_HEIGHT,
                 MESSAGE_VALID_HEIGHT));
 
         // empty username field specified
-        assertParseFailure(parser," username/", String.format(MESSAGE_INVALID_USERNAME,
+        assertParseFailure(parser, " username/", String.format(MESSAGE_INVALID_USERNAME,
                 MESSAGE_VALID_USERNAME));
 
         // empty weight field specified
-        assertParseFailure(parser," weight/", String.format(MESSAGE_INVALID_WEIGHT,
+        assertParseFailure(parser, " weight/", String.format(MESSAGE_INVALID_WEIGHT,
                 MESSAGE_VALID_WEIGHT));
     }
 
