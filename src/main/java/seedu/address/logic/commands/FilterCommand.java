e042caab95341cea69e0d75663d62a2d1c08d121
yicheng12 <2731705945zuigao@gmail.com>
2018-11-07 23:08:08 +0800

    edit comment on filter and sort command

--- src/main/java/seedu/address/logic/commands/FilterCommand.java
+++ src/main/java/seedu/address/logic/commands/FilterCommand.java
@@ -13,7 +13,7 @@ import seedu.address.model.workout.EquipmentPredicate;
 import seedu.address.model.workout.TypePredicate;
 
 /**
- * WIP
+ * filter the workouts with duration, type and equipment
  */
 public class FilterCommand extends Command {
     public static final String COMMAND_WORD = "filter";
