71c1c9da30a4dfd5c30568efcdc9fb97d6ed5dba
sijieg <dindis123@hotmail.com>
2018-11-09 20:09:48 +0800

    fix travis errors

--- src/main/java/seedu/address/model/workout/Workout.java
+++ src/main/java/seedu/address/model/workout/Workout.java
@@ -80,7 +80,9 @@ public class Workout {
         return instruction;
     }
 
-    public Remark getRemark() { return remark; }
+    public Remark getRemark() {
+        return remark;
+    }
 
 
     /**
