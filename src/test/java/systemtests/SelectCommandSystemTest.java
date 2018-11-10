6115bcbc3c66e07b37852035cfe178275607cb63
garyheng <E0026809@u.nus.edu>
2018-11-11 01:58:10 +0800

    Fix errors in SelectCommandSystemTest

--- src/test/java/systemtests/SelectCommandSystemTest.java
+++ src/test/java/systemtests/SelectCommandSystemTest.java
@@ -2,7 +2,6 @@ package systemtests;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_PARAMETER_DISPLAYED_INDEX;
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_WORKOUT_DISPLAYED_INDEX;
 import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import static seedu.address.logic.commands.SelectCommand.MESSAGE_SELECT_PARAMETER_SUCCESS;
 import static seedu.address.testutil.TestUtil.getLastParameterIndex;
